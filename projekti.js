86c0f86b3781138eb7eac9983d8e78dc9bd30b24
aleksandarm03 <accam003@gmail.com>
2025-03-14 20:35:40 +0100

    Dodat novi projekat

--- projekti.js
+++ projekti.js
@@ -1,24 +1,19 @@
-// script.js
+// Filter Projekata
+const filterButtons = document.querySelectorAll('.filter-button');
+const projectCards = document.querySelectorAll('.project-card');
 
+filterButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const filter = button.dataset.filter;
 
-    // Filter Projekata
-    const filterButtons = document.querySelectorAll('.filter-button');
-    const projectCards = document.querySelectorAll('.project-card');
+        // Update aktivnog dugmeta
+        filterButtons.forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
 
-    filterButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            const filter = button.dataset.filter;
-            
-            // Update aktivnog dugmeta
-            filterButtons.forEach(btn => btn.classList.remove('active'));
-            button.classList.add('active');
-
-            // Filter kartica
-            projectCards.forEach(card => {
-                const category = card.dataset.category;
-                card.style.display = (filter === 'all' || category === filter) ? 'block' : 'none';
-            });
+        // Filter kartica
+        projectCards.forEach(card => {
+            const categories = card.dataset.category.split(' '); // Razdvajanje kategorija po razmaku
+            card.style.display = (filter === 'all' || categories.includes(filter)) ? 'block' : 'none';
         });
     });
-
-  
+});
